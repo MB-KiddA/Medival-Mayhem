a2b5166d52a90fc6f63eb03e467a3c642bef25f8
Goofy Goober <91341620+MB-KiddA@users.noreply.github.com>
2023-05-26 12:53:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,16 +12,33 @@ def icons():
     if select < 1: # Makes Select Loop
         select = 4
 
-
-
-def icon_movement(keys_pressed, icons):
+def p1_icon_movement(keys_pressed, p1):
 
     if keys_pressed[pygame.K_a]: #If a is pressed and doesn't go past selection
-        select = select - 1 and pygame.time.wait(50)
+        select = select - 1 and pygame.time.wait(150)
     if keys_pressed[pygame.K_d]: #If d is pressed and doesn't go past slection
-        select = select + 1 and pygame.time.wait(50)
+        select = select + 1 and pygame.time.wait(150)
+    if keys_pressed[pygame.K_SPACE]:
+        pick = 1
+
+def p2_icon_movement(keys_pressed, p2):
+
+    if keys_pressed[pygame.K_LEFT]:
+        Select = Select - 1 and pygame.time.wait(150)
+    if keys_pressed[pygame.K_RIGHT]:
+        Select = Select + 1 and pygame.time.wait(150)
+    if keys_pressed
 
 def char_select():
+    pick = 0
     keys_pressed = pygame.key.get_pressed()
-    icon_movement(keys_pressed, icons)
+    while pick == 0:
+        p1_icon_movement(keys_pressed, p1)
 
+    while pick == 1:
+        p2_icon_movement(keys_pressed, p2)
+    
+    while pick == 2:
+        main()
+
+def main():
